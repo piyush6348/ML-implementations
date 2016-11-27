b6ec967f3eebb830b258517d0287e74f2fa75fd8
dell <piyush6348@gmail.com>
2016-11-27 22:54:01 +0530

    Neural Network part 1

---
+++ Neural Networks-1/Examples/LargestElementInEachMatrixRowExample/LargestElementInEachMatrixRowExample.m
@@ -0,0 +1,9 @@
+%% Largest Element in Each Matrix Row
+% Create a matrix and compute the largest element in each row.  
+
+% Copyright 2015 The MathWorks, Inc.
+
+A = [1.7 1.2 1.5; 1.3 1.6 1.99]
+
+%%
+M = max(A,[],2)
